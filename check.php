405ec2bf4e6db992c247c64146cfa405ca40f582
Kota Yamazaki <y4aiuo23@gmail.com>
2019-07-25 10:34:23 +0800

    おとい合わせフォーム

--- check.php
+++ check.php
@@ -1,13 +1,30 @@
 <?php
    $nickname = $_POST['nickname'];
-   echo $nickname;
+   if ($nickname == '') {
+      $nickname_result = 'ニックネームが入力されていません。';
+  } else {
+      $nickname_result = 'ようこそ、' . $nickname .'様';
+  }
+  echo $nickname_result; 
    echo '<br>';
    
    $email = $_POST['email'];
-   echo $email;
+   if ($email == '') {
+      $email_result = 'メールアドレスが入力されていません。';
+  } else {
+      $email_result = 'メールアドレス：' . $email;
+  }
+  echo $email_result;
    echo '<br>';
+
    $content = $_POST['content'];
-   echo $content;
+   if ($content == '') {
+      $content_result =  'お問い合わせ内容が入力されていません。';
+  } else {
+      $content_result = 'お問い合わせ内容：' . $content;
+  }
+  echo $content_result;
+   echo '<br>';
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -17,5 +34,21 @@
 </head>
 <body>
    <h1>入力内容確認</h1>
+ 
+
+   <form method="POST" action="thanks.php">
+   <input type="hidden" name="nickname" value="<?php echo $nickname; ?>">
+        <input type="hidden" name="email" value="<?php echo $email; ?>">
+        <input type="hidden" name="content" value="<?php echo $content; ?>">
+        <input type="button" value="戻る" onclick="history.back()">
+        <?php if ($nickname != '' && $email != '' && $content != ''): ?>
+        <input type="submit" value="OK">
+
+        <?php endif; ?>
+
+
+   
+
+</form>
 </body>
 </html>
